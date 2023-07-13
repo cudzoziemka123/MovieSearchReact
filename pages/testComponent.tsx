047a11e05ca2d6c30c58f44080c444651372ff66
Alisa Borkowska <alisa.borkowska@gmail.com>
2023-07-13 19:18:04 +0200

    REC-7-useState-hook

--- pages/testComponent.tsx
+++ pages/testComponent.tsx
@@ -1,7 +1,16 @@
+'use client'
+import {useState, useEffect} from 'react';
+
 const TestComponent = () => {
+    const [myState, setMyState] = useState(0);
+    useEffect(()=>{
+        setMyState(8);
+    },[]);
+    
     return (
         <>
         Hello
+        {myState}
         </>
     );
 }
